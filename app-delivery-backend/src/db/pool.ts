136e4554365998e31b65e0f410e784f6c34a0b00
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-08-29 21:09:41 -0300

    implementando para rodar com docker

--- app-delivery-backend/src/db/pool.ts
+++ app-delivery-backend/src/db/pool.ts
@@ -1,14 +1,10 @@
+import 'dotenv/config';
 import pg from 'pg';
-require('dotenv').config();
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.user,
-  password: process.env.password,
-  host: process.env.host,
-  port: Number(process.env.port),
-  database: process.env.database
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;

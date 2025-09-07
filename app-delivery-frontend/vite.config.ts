7e97f4821de7c8dd923676dc4762af3df3b55cbe
Lucas Lucena <lucasandradelucaslucas123@gmail.com>
2025-09-07 15:30:40 -0300

    Atualizando DockerFile

--- app-delivery-frontend/vite.config.ts
+++ app-delivery-frontend/vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Isso faz o Vite escutar em todos os endereços de rede,
+    // tornando-o acessível de fora do contêiner Docker.
+    host: true,
+    port: 5173, // Garante que a porta é a 5173
+    // Essa configuração é crucial para o Hot Reload (atualização automática) funcionar corretamente com Docker.
+    watch: {
+      usePolling: true
+    }
+  }
 })
